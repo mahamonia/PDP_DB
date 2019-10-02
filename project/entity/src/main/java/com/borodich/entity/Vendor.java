71f3386a5c53fd9b9c996f33e7b54000a91df343
borodich_tania <barodzich_tatsiana_wp@senla.eu>
2019-10-02 14:22:38 +0300

    no message

--- project/entity/src/main/java/com/borodich/entity/Vendor.java
+++ project/entity/src/main/java/com/borodich/entity/Vendor.java
@@ -1,9 +1,7 @@
 package com.borodich.entity;
 
 import com.borodich.entity.api.AbstractBaseEntity;
-import com.fasterxml.jackson.annotation.JsonIdentityInfo;
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 
 import java.util.List;
 
